11c14797dac1ad4e83aba4bd24df3d83082ec618
Landy Lopez <landuchilopez@gmail.com>
2019-11-13 11:28:41 -0600

    Version final 1.0

--- Views/home/index.php
+++ Views/home/index.php
@@ -14,11 +14,12 @@
 
   <div class="container">
     <div class="row">
-      <div class="col-lg-12 text-center">
+      <div class="col-lg-12 text-center" style="margin-top:40px;margin-bottom:200px;">
         <h1 class="mt-5">Bienvenido al sitio</h1>
         <p class="lead">Este sitio fue creado para llevar un control y registro de lo posibles
-		        fallos que puedan presentarse en su platadorma en el día a día, con la finalidad de brindar
+		        fallos que puedan presentarse en su plataforma en el día a día, con la finalidad de brindar
             un buen servicio técnico en el área de sitemas y atender de forma oportuna cada uno de ellos.</p>
+        <img src="Content/img/fondo.png" class="img-fluid" alt="Responsive image">
       </div>
       </div>
     </div>
